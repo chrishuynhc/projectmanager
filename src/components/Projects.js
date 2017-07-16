1f258a09f77fa708bec4dda1bc64a50258aab308
Chris <chris.huynh.c@gmail.com>
2017-07-15 22:03:49 -0500

    First Commit

---
+++ src/components/Projects.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        My Apps
+      </div>
+    );
+  }
+}
+
+export default App;
