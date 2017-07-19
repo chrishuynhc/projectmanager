efb7ad5716c5cb7c65594fd845ef4a4eb14e8307
Chris <chris.huynh.c@gmail.com>
2017-07-18 23:44:12 -0500

    Added components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,49 @@
 import React, { Component } from 'react';
 import './App.css';
 import Projects from './components/Projects';
+import AddProject from './components/AddProject';
+import uuid from 'uuid';
 
 class App extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      projects:[]
+    }
+  }
+
+  componentWillMount(){
+    this.setState({projects: [
+        {
+          id: uuid.v4(),
+          title: 'Business Website',
+          category: 'Web Design'
+        },
+        {
+          id: uuid.v4(),
+          title: 'Social App',
+          category: 'Mobile Development'
+        },
+        {
+          id: uuid.v4(),
+          title: 'Ecommerce Shopping Cart',
+          category: 'Web Development'
+        }
+    ]});
+  }
+
+  handleAddProject(project){
+    let projects = this.state.projects;
+    projects.push(project);
+    this.setState({projects: projects});
+  }
+
   render() {
     return (
       <div className="App">
-        My App
-        <Projects />
+        <AddProject addProject={this.handleAddProject.bind(this)}/>
+        <Projects projects={this.state.projects}/>
       </div>
     );
   }
